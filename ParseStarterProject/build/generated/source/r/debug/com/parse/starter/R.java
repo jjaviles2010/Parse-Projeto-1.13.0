8fdd0206fe97ea4e882bef3a2ea2134bc7fb3b55
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2018-09-06 22:47:11 -0300

    Adding app icon.

--- ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -3945,7 +3945,8 @@ public final class R {
     public static final int menu_main=0x7f0b0000;
   }
   public static final class mipmap {
-    public static final int ic_launcher=0x7f0c0000;
+    public static final int ic_instagram=0x7f0c0000;
+    public static final int ic_launcher=0x7f0c0001;
   }
   public static final class string {
     public static final int HintTxtEmail=0x7f0d0000;
